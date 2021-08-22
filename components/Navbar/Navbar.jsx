a06c3cc32c1fb891a6f3cd2209650d877307569c
Edgarvar <edgardo_vargas@outlook.com.ar>
2021-08-22 03:30:51 -0300

    Navbar Desktop & Fuentes

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
+import Link from 'next/link';
+import styles from '../../styles/components/Navbar.module.css';
+
+
 export default function Navbar() {
+
+    
+
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <header id="header" className={styles.header}>
+            {/* <div className={styles.toggle}></div> */}
+            <div id="navbar" className={styles.navbar}>
+                <ul className={styles.ul}>
+                    <li className="barlow-semibold"><Link href="/"><a className={styles.a}>HOME</a></Link></li>
+                    <li className="barlow-semibold"><Link href="/quienes-somos"><a  className={styles.a}>QUIENES SOMOS</a></Link></li>
+                    <li className="barlow-semibold">
+                        <Link href="/productos"><a className={styles.a}>PRODUCTOS</a></Link>
+                        <div>
+                            <ul>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="barlow-semibold"><Link href="/contacto"><a className={styles.a}>CONTACTO</a></Link></li>
+                </ul>
+            </div>
+        </header>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -109,7 +109,7 @@ export default function Home() {
         <categories-list></categories-list>
       </section>
 
-      <app-footer></app-footer>
+      {/* <app-footer></app-footer> */}
     </Layout>
   )
 }
