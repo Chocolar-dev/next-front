f6278cf52945f98dafabc20c4eeaf871b7c36218
Edgarvar <edgardo_vargas@outlook.com.ar>
2021-08-26 05:35:10 -0300

    category page + card product

---
+++ components/CardProduct/CardProduct.jsx
@@ -0,0 +1,20 @@
+export default function CardProduct() {
+    return (
+        <div className="card-product">
+            <h3>ALCALINO <strong>LEVE</strong></h3>
+            <div className="img-product">
+                <img src="\old_assets\panel\productos\16_54549drn.jpg" alt="" />
+            </div>
+            <div className="detail">
+                <h2>CACAO EN POLVO</h2>
+                <h4>ALCALINO BARRI RL1</h4>
+                <p>BOLSA DE CACAO EN POLVO CONTENIDO NETO 25KG</p>
+                <h5>BARRY CALLEBAUT</h5>
+            </div>
+            <div className="btns">
+                <button className="btn1">VER MÁS</button>
+                <button className="btn2">SOLICITAR INFORMACIÓN</button>
+            </div>
+        </div>
+    )
+}
