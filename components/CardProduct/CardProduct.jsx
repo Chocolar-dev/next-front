44dcfb2c14b509f0eee65f9ef72a25198272401f
Edgarvar <edgardo_vargas@outlook.com.ar>
2021-08-27 03:24:55 -0300

    Category pages mobile

--- components/CardProduct/CardProduct.jsx
+++ components/CardProduct/CardProduct.jsx
@@ -1,19 +1,41 @@
 export default function CardProduct() {
     return (
         <div className="card-product">
-            <h3>ALCALINO <strong>LEVE</strong></h3>
-            <div className="img-product">
-                <img src="\old_assets\panel\productos\16_54549drn.jpg" alt="" />
+            <div className="card-desk">
+                <h3>ALCALINO <strong>LEVE</strong></h3>
+                <div className="img-product">
+                    <img src="\old_assets\panel\productos\16_54549drn.jpg" alt="" />
+                </div>
+                <div className="detail">
+                    <h2>CACAO EN POLVO</h2>
+                    <h4>ALCALINO BARRI RL1</h4>
+                    <p>BOLSA DE CACAO EN POLVO CONTENIDO NETO 25KG</p>
+                    <h5>BARRY CALLEBAUT</h5>
+                </div>
+                <div className="btns">
+                    <button className="btn1">VER MÁS</button>
+                    <button className="btn2">SOLICITAR INFORMACIÓN</button>
+                </div>
             </div>
-            <div className="detail">
-                <h2>CACAO EN POLVO</h2>
-                <h4>ALCALINO BARRI RL1</h4>
-                <p>BOLSA DE CACAO EN POLVO CONTENIDO NETO 25KG</p>
-                <h5>BARRY CALLEBAUT</h5>
-            </div>
-            <div className="btns">
-                <button className="btn1">VER MÁS</button>
-                <button className="btn2">SOLICITAR INFORMACIÓN</button>
+            <div className="card-mobile">
+                <div className="img-product">
+                    <img src="\old_assets\panel\productos\16_54549drn.jpg" alt="" />
+                </div>
+                <div className="type">
+                    <h3>ALCALINO LEVE</h3>
+                </div>
+                <div className="wrapper-detail">
+                    <div className="detail">
+                        <h2>CACAO EN POLVO</h2>
+                        <h4>ALCALINO BARRI RL1</h4>
+                        <p>BOLSA DE CACAO EN POLVO CONTENIDO NETO 25KG</p>
+                        <h5>BARRY CALLEBAUT</h5>
+                    </div>
+                    <div className="btns">
+                        <button className="btn1">VER MÁS</button>
+                        <button className="btn2">SOLICITAR INFORMACIÓN</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
